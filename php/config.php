149231c952dba292c06e6cc89238870cc10a0611
paiva <134117840+joaothearthefato@users.noreply.github.com>
2026-03-13 11:36:37 -0300

    atualizacao marcao 0.03

---
+++ php/config.php
@@ -0,0 +1,20 @@
+<?php
+// Configurações do banco de dados
+$host = "localhost";   // Servidor
+$usuario = "root";     // Usuário do banco
+$senha = "Home@spSENAI2025!";           // Senha do banco
+$banco = "oyama_hub";  // Nome do banco
+
+// Criar conexão
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+echo "Conexão realizada com sucesso!";
+
+// Fechar conexão
+$conn->close();
+?>
