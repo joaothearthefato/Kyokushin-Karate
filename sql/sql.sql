a27d08de70a128bd1ffcf1425692f831b88f1f3a
paiva <134117840+joaothearthefato@users.noreply.github.com>
2026-03-26 14:13:43 -0300

    Implementa área do aluno e CRUD de katas para sensei

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,32 +1,44 @@
+CREATE DATABASE oyama_hub;
+USE oyama_hub;
 
-create database Oyama_Hub;
--- Criação da tabela de usuários
-CREATE TABLE usuarios_oyama (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    tipo ENUM('aluno', 'administrador') NOT NULL,
-    faixa_id INT,
-    FOREIGN KEY (faixa_id) REFERENCES faixas(id)
+CREATE TABLE faixas (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  ordem INT NOT NULL
 );
 
--- Criação da tabela de katas
+CREATE TABLE usuarios_oyama (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  tipo ENUM('aluno', 'administrador') NOT NULL,
+  idade INT NULL,
+  altura DECIMAL(5,2) NULL,
+  peso DECIMAL(5,2) NULL,
+  faixa_id INT NULL,
+  FOREIGN KEY (faixa_id) REFERENCES faixas(id)
+);
+
 CREATE TABLE katas (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT NOT NULL,
-  video VARCHAR(255),
+  video VARCHAR(255) NULL,
+  foto VARCHAR(255) NULL,
   nivel_dificuldade ENUM('iniciante', 'intermediario', 'avancado') NOT NULL
 );
 
-CREATE TABLE faixas (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    ordem INT NOT NULL -- Ex: 1 para Branca, 2 para Amarela, etc.
+CREATE TABLE treinos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT NOT NULL,
+  data_treino DATE NOT NULL,
+  descricao TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (usuario_id) REFERENCES usuarios_oyama(id) ON DELETE CASCADE
 );
 
-INSERT INTO faixas (nome, ordem) VALUES 
+INSERT INTO faixas (nome, ordem) VALUES
 ('Branca (Iniciante)', 1),
 ('Laranja (10º Kyu)', 2),
 ('Azul (8º Kyu)', 3),
@@ -35,7 +47,3 @@ INSERT INTO faixas (nome, ordem) VALUES
 ('Marrom (2º Kyu)', 6),
 ('Marrom com Ponta Preta (1º Kyu)', 7),
 ('Preta (1º Dan)', 8);
-
-select * from usuarios_oyama;
-select * FROM FAIXAS;
-	
