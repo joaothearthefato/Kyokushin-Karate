8c73e19ab6b063801fe7173ba52de126fd3cdc90
gustavohmartins10-cpu <gustavo.h.martins10@aluno.senai.br>
2026-03-27 09:52:05 -0300

    dashboard.css

--- php/dashboard.php
+++ php/dashboard.php
@@ -12,19 +12,19 @@ require 'config.php';
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Oyama Hub | Dashboard</title>
-<link rel="icon" href="./img/kyokushinicon.png">
-<link rel="stylesheet" href="css/style.css">
+<link rel="icon" href="../img/kyokushinicon.png">
+<link rel="stylesheet" href="../css/dashboard.css">
 <meta name="description" content="Treine Karate Kyokushin e desenvolva força, disciplina e superação.">
 </head>
 <body>
 
 <section class="navbarArea">
 <div class="header">
-<a href="Inicio">Inicio</a>
-<a href="Inicio">Progresso</a>
-<a href="Inicio">Katas</a>
-<a href="Inicio">Kihon</a>
-<a href="Inicio">Treinos</a> 
+<a href="../index.html">Inicio</a>
+<a href="../dashboard/progresso.php">Progresso</a>
+<a href="../dashboard/katas.php">Katas</a>
+<a href="../dashboard/kihon.php">Kihon</a>
+<a href="../dashboard/treinos.php">Treinos</a> 
  <a href="../php/logout.php"> <button class="logout-btn">Logout</button></a>
 </div>
 </section>
@@ -35,32 +35,37 @@ echo $_SESSION['nome']; ?>
 !</h1>
 </div>
 
-<container class="dashboard-container">
+<div class="dashboard-container">
 <section class="dashboard-section">
   <a href="../dashboard/progresso.php" class="dashboard-link">
     <div class="dashboard-card">
       <h2>Progresso</h2>
       <p>Acompanhe seu progresso e conquistas no Karate Kyokushin.</p>
     </div>
+  </a>
 
-    <a href="../dashboard/katas.php" class="dashboard-link">
+  <a href="../dashboard/katas.php" class="dashboard-link">
     <div class="dashboard-card">
       <h2>Katas</h2>
       <p>Explore os katas e aprimore suas técnicas de forma estruturada.</p>
     </div>
-    <a href="../dashboard/kihon.php" class="dashboard-link">
+  </a>
+
+  <a href="../dashboard/kihon.php" class="dashboard-link">
     <div class="dashboard-card">
       <h2>Kihon</h2>
       <p>Domine os fundamentos do Karate Kyokushin com os exercícios de Kihon.</p>
     </div>
+  </a>
 
-    <a href="../dashboard/treinos.php" class="dashboard-link">
+  <a href="../dashboard/treinos.php" class="dashboard-link">
     <div class="dashboard-card">
       <h2>Treinos</h2>
       <p>Encontre treinos personalizados para aprimorar suas habilidades no Karate Kyokushin.</p>
-</div>
+    </div>
+  </a>
 </section>
-</container>
+</div>
 
 
 </body>
